122702ce5b168a4b8b17ba5ba7a3f52cdb1ea06e
rahulbera9386 <rahulberastd2002@gmail.com>
2023-08-03 17:24:46 +0530

    Rahul

---
+++ src/components/footer/footerp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./footer.css";
+class footerp extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Created By Rahul Bera</h3>
+      </div>
+    );
+  }
+}
+
+export default footerp;
